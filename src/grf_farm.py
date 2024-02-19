549a9de7aa5d338d2ecc07ece8e5b3d4e7fa4bd0
Andy <andy@teamrubber.com>
2024-02-19 17:33:51 +0000

    Change: support Iron Ibex

--- src/grf_farm.py
+++ src/grf_farm.py
@@ -3,7 +3,7 @@ currentdir = os.curdir
 
 dist_container_path = os.path.join(currentdir, 'dist')
 
-registered_grfs = ['firs', 'iron-horse', 'road-hog', 'unsinkable-sam']
+registered_grfs = ['firs', 'iron-horse', 'iron-ibex', 'road-hog', 'unsinkable-sam']
 
 # chameleon templating goes faster if a cache dir is used; this specifies which dir is cache dir
 chameleon_cache_dir = '.chameleon_cache'
---
+++ src/iron-ibex/test-19-feb-2024/readme.txt
@@ -0,0 +1,15 @@
+==================================
+Iron Ibex test-19-feb-2024
+==================================
+Trains, for OpenTTD
+
+Iron Ibex docs are published online:
+  https://grf.farm/iron-horse/test-19-feb-2024/index.html
+
+=======
+GRF Name: Iron Ibex
+
+This version of Iron Ibex is created using repository revision 6982
+
+Thanks to *all* who helped create Iron Ibex.
+
